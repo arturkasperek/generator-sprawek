c29be66cde0f363d5bbd2a1b0fe5d19594d34f91
Artur <arturmiks@gmail.com>
2018-03-16 12:28:43 +0100

    Initial version

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,12 +12,15 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import { Button } from 'react-bootstrap';
+
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <h1>
-        <FormattedMessage {...messages.header} />
+        This is some component ;)
+        <Button bsStyle="success">Click me!</Button>
       </h1>
     );
   }
